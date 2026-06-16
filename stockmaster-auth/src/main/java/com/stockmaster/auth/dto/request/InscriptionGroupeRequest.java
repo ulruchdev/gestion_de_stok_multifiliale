24ff00634b3703837bbc03c64a0bca811da88a54
ulrich dev <ulruchdev@gmail.com>
2026-06-16 05:06:20 +0200

    fix(GS-012): align password regex across all DTOs (require upper + lower + digit + special)

--- stockmaster-auth/src/main/java/com/stockmaster/auth/dto/request/InscriptionGroupeRequest.java
+++ stockmaster-auth/src/main/java/com/stockmaster/auth/dto/request/InscriptionGroupeRequest.java
@@ -49,7 +49,7 @@ public class InscriptionGroupeRequest {
     @NotBlank(message = "Le mot de passe est obligatoire")
     @Size(min = 8, max = 50, message = "Le mot de passe doit contenir entre 8 et 50 caractères")
     @Pattern(
-        regexp = "^(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
         message = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial"
     )
     private String motDePasse;
