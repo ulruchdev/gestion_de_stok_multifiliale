cea66de8c16bdcca0b8bc76f822afe955e3079ef
ulrich dev <ulruchdev@gmail.com>
2026-07-31 15:59:39 +0200

    fix(GS-000): elargir les caracteres speciaux acceptes dans le mot de passe
    Le pattern de validation ne reconnaissait que 7 caracteres speciaux
    (@#$%^&+=!). Elargi a un jeu plus large et standard
    (!@#$%^&*()_+={}[]|:;<>,.?/~`-) dans les 4 DTOs concernes
    (inscription entreprise unique, inscription groupe, reset password,
    change password) pour ne pas rejeter des mots de passe legitimes.

--- backend/stockmaster-auth/src/main/java/com/stockmaster/auth/dto/request/ResetPasswordRequest.java
+++ backend/stockmaster-auth/src/main/java/com/stockmaster/auth/dto/request/ResetPasswordRequest.java
@@ -14,7 +14,7 @@ public class ResetPasswordRequest {
     @NotBlank(message = "Le nouveau mot de passe est obligatoire")
     @Size(min = 8, max = 50, message = "Le mot de passe doit contenir entre 8 et 50 caractères")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+={}\\[\\]|:;<>,.?/~`-]).{8,}$",
             message = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial"
     )
     private String nouveauMotDePasse;
